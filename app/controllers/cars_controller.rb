dc4fb17efeb233003b4da73923b980a062629757
Ahmed Elmi <ahmedelmi11@gmail.com>
2018-02-27 14:22:32 +0000

    did the car controller crud

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,28 +1,54 @@
 class CarsController < ApplicationController
   def index
+    @cars = Cars.all
   end
 
   def show
+    @car = Car.find(params[:id])
   end
 
 
   def new
+    @car = Car.new
   end
 
 
   def create
+    @car = Car.new(cars_params)
+    if @car.save
+      redirect_to cars_path
+    else
+      render :new
+    end
   end
 
 
   def edit
+    @car = Car.find(params[:id])
   end
 
   def update
+    @car = Car.find(params[:id])
+  if @car.update(cars_params)
+    redirect_to cars_path
+  else
+    render :new
+  end
   end
 
 
   def destroy
+     @car = Car.find(params[:id])
+  if @car.destroy
+    redirect_to cars_path
+  else
+    render :new
+  end
   end
 
+            private
+  def cars_params
+    params.require(:car).permit(:user_id, :make, :year, :model, :price_per_day, :story, :available, :photos, )
+  end
 
 end
