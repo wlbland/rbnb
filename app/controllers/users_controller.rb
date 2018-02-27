257091d10326183c676816a36dd76a4126ec9618
William Bland <williamlbland@yahoo.co.uk>
2018-02-27 15:11:05 +0000

    working on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,4 @@
 class UsersController < ApplicationController
 
-  def index
-  end
-
-
-
-  def show
-  end
-
-
-  def new
-  end
-
-  def create
-  end
-
-
-  def edit
-  end
-
-
-  def update
-  end
-
-
-
+  def inde
 end
