c6679b82de6f279a2f95dd1240be86c03a5a2fe0
William Bland <williamlbland@yahoo.co.uk>
2018-02-27 17:16:58 +0000

    corrected a problem with the pundit

--- app/policies/car_policy.rb
+++ app/policies/car_policy.rb
@@ -9,8 +9,11 @@ class CarPolicy < ApplicationPolicy
     end
   end
 
+  def create?
+    return true
+  end
 
- def update?
+  def update?
     record.user == user
     # - record: the restaurant passed to the `authorize` method in controller
     # - user:   the `current_user` signed in with Devise.
