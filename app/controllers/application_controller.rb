8f7eb70863049b2d652d567a8102f133dd3df95e
William Bland <williamlbland@yahoo.co.uk>
2018-02-28 16:42:14 +0000

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
 
   # after_action :verify_authorized, except: :index, unless: :skip_pundit?
   # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
-   def after_sign_in_path_for(scope)
-      new_car_path
-   end
+  #  def after_sign_up_path_for(scope)
+  #     new_car_path
+  #  end
 
-  private
+  # private
 
   # def skip_pundit?
   #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
