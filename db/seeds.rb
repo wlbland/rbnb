55c95bdc2c9b0f86f88e0fe1b48520a8d9046f90
David O'Neill daveyoneill123@gmail.com <daveyoneill123@gmail.com>
2018-03-01 16:38:33 +0000

    fixed the cloudinary link on the dashboard and deleted broken seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 
 
-new_user = User.create!(email:"a@a.com")
+# new_user = User.create!(email:"a@a.com")
 
 
-20.times do
+# 20.times do
 
-  Car.create!(make: ["Renault", "BMW", "VW", "Mini Cooper", "Aston Martin", "Porsche", "Lamborghini"].sample,
-              model: Faker::Lorem.sentence(2, false, 0).chop,
-              year: (1990..2009).to_a.sample,
-              price_per_day: (19..2000).to_a.sample,
-              available: [true, false].sample,
-              story: Faker::Lorem.paragraph,
-              photo: Faker::Internet.url,
-              user_id: new_user.id)
-end
+#   Car.create!(make: ["Renault", "BMW", "VW", "Mini Cooper", "Aston Martin", "Porsche", "Lamborghini"].sample,
+#               model: Faker::Lorem.sentence(2, false, 0).chop,
+#               year: (1990..2009).to_a.sample,
+#               price_per_day: (19..2000).to_a.sample,
+#               available: [true, false].sample,
+#               story: Faker::Lorem.paragraph,
+#               photo: Faker::Internet.url,
+#               user_id: new_user.id)
+# end
 
