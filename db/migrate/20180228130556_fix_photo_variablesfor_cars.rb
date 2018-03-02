65eeefed77f5588a4d59e101c77c2c983716684f
William Bland <williamlbland@yahoo.co.uk>
2018-03-02 17:00:46 +0000

    done

--- db/migrate/20180228130556_fix_photo_variablesfor_cars.rb
+++ db/migrate/20180228130556_fix_photo_variablesfor_cars.rb
@@ -4,6 +4,6 @@ class FixPhotoVariablesforCars < ActiveRecord::Migration[5.1]
     add_column :cars, :interior_image_1, :string
     add_column :cars, :interior_image_2, :string
     add_column :cars, :interior_image_3, :string
-    remove_column :cars, :photos, :string
+    # remove_column :cars, :photos, :string
   end
 end
--- db/migrate/20180228164259_add_photo.rb
+++ db/migrate/20180228164259_add_photo.rb
@@ -1,5 +1,5 @@
 class AddPhoto < ActiveRecord::Migration[5.1]
   def change
-    add_column :cars, :photo_url, :string
+    add_column :cars, :photo, :string
   end
 end
