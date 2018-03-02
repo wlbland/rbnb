0116cf434911c769d4c1642aced3471a729112db
William Bland <williamlbland@yahoo.co.uk>
2018-03-02 14:39:58 +0000

    removed filter replaced with search

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,13 @@ class Car < ApplicationRecord
   belongs_to :user
   validates :make, presence: true
   mount_uploader :photo, PhotoUploader
+
+  include PgSearch
+
+  pg_search_scope :search_by_make_and_model,
+    against: [:make, :model ],
+    using: {
+      tsearch: {prefix: true }
+    }
+
 end
